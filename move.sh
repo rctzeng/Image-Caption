d1b966a48fa2641260aab530e5f5077b3b7b918e
Ruochun Tzeng <rubys88684@gmail.com>
2017-06-21 23:28:28 +0800

    update move.sh

--- move.sh
+++ move.sh
@@ -1,5 +1,5 @@
 unzip dataset.zip
-unzip pre-trained-w2v-VGG16
+unzip pre-trained-w2v-VGG16.zip
 mv glove.6B.100d.txt pre_trained
 mv vgg16_mat.pkl pre_trained
 rm -rf pre-trained-w2v-VGG16* dataset.zip
